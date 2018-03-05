4449664a9de0b1f46d51fc36f1d951a2fed57f8e
Kris <kris.bierma@gmail.com>
2018-03-05 10:30:08 -0600

    on click working again. Grr.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,15 +8,20 @@ var apiKey="8HHV4xr4WJIrV4CnbfW7pYyIK2NuADEc";
 var picBox;
 
 //starts page with buttons for words already in the array
-for (var i=0; i<elements.length; i++){
-    var button = $("<button class='btn btn-element btn-info'>" + elements[i] + "</button>");
-    button.attr("data-word", elements[i]);
-    $("#buttons").append(button);
-    console.log(button);
+function renderButton(){
+    $("#buttons").empty();
+    for (var i=0; i<elements.length; i++){
+        var button = $("<button class='btn btn-element btn-info'>" + elements[i] + "</button>");
+        button.attr("data-word", elements[i]);
+        $("#buttons").append(button);
+        console.log(button);
+    };
 };
 
 //click button to get gifs
-$(".btn-element").on("click", function(event){
+// $(".btn-element").on("click", function(event){
+function getGifs(){
+    console.log("working");
     $("#gifs").empty();
     // ex query: var queryURL = "https://api.giphy.com/v1/gifs/search?q=cats&limit=1&rating=g&api_key=8HHV4xr4WJIrV4CnbfW7pYyIK2NuADEc";
     //call on the data attr from button pushed
@@ -30,45 +35,38 @@ $(".btn-element").on("click", function(event){
             var picRating = response.data[j].rating;
             var pic = ("<img src=" +response.data[j].images.downsized_still.url+">");
             picBox = ("<div class='picBox'>Rating: "+ picRating.toUpperCase() + "<br>" + pic);        
-            picBox.attr("data-name", "img src=" +response.data[j].images.downsized.url);
+            // picBox.attr("data-name", "img src=" +response.data[j].images.downsized.url);
             $("#gifs").append(picBox);
         };
     });
+// }); //end click function
+};
 
-}); //end click function
+$("#newWord").on("click", function(event){
+    event.preventDefault();
+    var anotherWord = $("#word-input").val().trim();
+    //uppercase first letter
+    elements.push(anotherWord);
+    // console.log(elements);
+    this.form.reset();
+    renderButton();
+});
+
+$(document).on("click", ".btn-element", getGifs); //from class
+
+// function alertMovieName(){
+//     console.log("working");
+// };
+
+renderButton();
+
+}); //end doc.ready
+
+
 
 //click gif to animate
 // $("#picBox").on("click", function(picBox){
     
 // });
 //click again to stop playing
-
-$("#newWord").on("click", function(event){
-    event.preventDefault();
-    var anotherWord = $("#word-input").val();
-    elements.push(anotherWord);
-    // console.log(elements);
-    this.form.reset();
-});
-
-// function addWordButton(){
-//     var button = $("<button class='btn btn-element btn-info'>" + elements[i] + "</button>");
-//     button.attr("data-word", elements[i]);
-//     $("#buttons").append(button);
-//     console.log(button);
-// };
-
-for (var i=0; i<elements.length; i++){
-    var button = $("<button class='btn btn-element btn-info'>" + elements[i] + "</button>");
-    button.attr("data-word", elements[i]);
-    $("#buttons").append(button);
-    console.log(button);
-};
-
-    //when submit is clicked, push new word to elements array
-    //create and append new button in button div
-
-}); //end doc.ready
-
-
 
