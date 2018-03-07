11775ae3212bde1054c3cd0c6982c36d3ebfc29f
Kris <kris.bierma@gmail.com>
2018-03-06 18:09:20 -0600

    finished html, css, js, readMe

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # GIF-API
+
+This simple app uses an API key to communicate with Giphy.com and AJAX to request and bring back the info to the page. The user can click a button to get ten gifs related to that word or can enter in a new word to create a new button.  
+
+## Instructions
+
+1. Click a blue word button to get 10 gifs.
+2. Type a new word and click submit to make a new button. 
+
+## Built With
+
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - Makes it interactive
+* [JQuery](http://jquery.com/) - Handles events
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - Makes it pretty
+* AJAX - Retrieves data without refreshing the page
+* API key - Communicates with Giphy
+
+## Author
+
+* **Kris Acker Bierma** - [KrisBierma](https://github.com/KrisBierma)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 
 //array for button words
-var elements = ["sun", "moon"];
-var limit = 1; //change to 10
+var elements = ["sun", "moon", "fire", "water", "rocks", "wind", "earth", "planets"];
+var limit = 10;
 var rating="pg";
 var apiKey="8HHV4xr4WJIrV4CnbfW7pYyIK2NuADEc";
-var picBox;
-var picStill;
-var picAnimated;
 
 //starts page with buttons for words already in the array
 function renderButton(){
@@ -16,18 +13,16 @@ function renderButton(){
         var button = $("<button class='btn btn-element btn-info'>" + elements[i] + "</button>");
         button.attr("data-word", elements[i]);
         $("#buttons").append(button);
-        console.log(button);
     };
 };
 
-//click button to get gifs
-// $(".btn-element").on("click", function(event){
 function getGifs(){
-    console.log("working");
     $("#gifs").empty();
-    // ex query: var queryURL = "https://api.giphy.com/v1/gifs/search?q=cats&limit=1&rating=g&api_key=8HHV4xr4WJIrV4CnbfW7pYyIK2NuADEc";
-    //call on the data attr from button pushed
+    
+    //call for the data attr from button pushed
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+($(this).attr("data-word"))+"&limit="+limit+"&rating="+rating+"&api_key="+apiKey;
+
+    //ajax sends request to api
     $.ajax({
         url:queryURL,
         method: "GET"
@@ -35,92 +30,49 @@ function getGifs(){
         console.log(response);
         for (var j =0; j<limit; j++){     
             var newDiv = $("<div>");
-            // newDiv.toggleClass("show");
-            // newDiv.addClass("green");
+            newDiv.addClass("picBox");
             newDiv.append("Rating: "+response.data[j].rating.toUpperCase());
-            newDiv.append("<img class='gifBtn' id='gifChange' src=" +response.data[j].images.downsized_still.url+">");
-            // newDiv.toggleClass("green, true");
-            (newDiv).append("<img class='gifBtnAnimated' src=" +response.data[j].images.original.url+">");
+
+            var newImage = $("<img>");
+            newImage.attr("src", response.data[j].images.fixed_height_still.url);
+            newImage.attr("data-still", response.data[j].images.fixed_height_still.url);
+            newImage.attr("data-animate", response.data[j].images.fixed_height.url);
+            newImage.addClass("gif");
+            newImage.attr("data-state", "still");
+            newDiv.append(newImage);
+
             $("#gifs").append(newDiv);
         };
     });
-// }); //end click function
 };
 
-$(document).on("click", ".gifBtn", function(e){
-    console.log(e);
-    var p= e.currentTarget.className;
-    console.log(p);
-    if (p.style.display=="none"){
-        p.style.display = "block";
+//click to change between animate and still gifs
+$(document).on("click", ".gif", function(){
+    var state = $(this).attr("data-state"); //this is the element that's clicked
+
+    if (state==="still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     }
     else{
-        p.style.display="none";
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-    // $(this).toggleClass("show");
-});
+}); 
 
-$(document).on("click", ".gifBtnAnimated", function(e){
-    // console.log(e);
-    // $(this).toggleClass("show");
-    var blah = $(this);
-    if (blah.style.display==="none"){
-        blah.style.display = "block";
-    }
-    else{
-        blah.style.display="none";
-    }
-});
-
-// $(document).on("click", function(event){
-//     console.log("hi");
-// }); 
-
-// function animateGif(){
-//     alert("Hi");
-// };
-
-// function animateGifs(){
-//     console.log("working");
-//     if (picStill){
-//         // console.log(picStill);
-//         $(picStill).removeClass("show"); 
-//         $(picStill).addClass("hide");
-//         console.log(picStill);
-
-    // }
-//     else if (picAnimated){
-//         console.log("else");
-//         picStill.addClass("hide");
-//         picAnimated.addClass("show");
-//     };
-
-//     // pic = ("<img src=" +response.data[j].images.original.url+">");
-// };
-
+//click submit button 
 $("#newWord").on("click", function(event){
     event.preventDefault();
-    var anotherWord = $("#word-input").val().trim();
-    //uppercase first letter
+    var anotherWord = $("#word-input").val().trim();//gets the value of the word typed, minus white spaces on the ends
+    if (anotherWord==="") return; //if nothing's typed, no blank button
     elements.push(anotherWord);
     this.form.reset();
     renderButton();
 });
 
-$(document).on("click", ".btn-element", getGifs); //from class
+//click a button to get get gifs with the function
+$(document).on("click", ".btn-element", getGifs);
 
-// $(document).on("click", "img", animateGifs);
-
-renderButton();
+renderButton(); //makes the buttons when the page loads
 
 }); //end doc.ready
-
-
-
-//click gif to animate
-// $("#picBox").on("click", function(picBox){
-    
-// });
-//click again to stop playing
-
-//add alt tags
